d23f36823091a8b362dbb9c64a32d97fff32e905
Abhishek <programwithabhi@gmail.com>
2025-11-09 19:25:53 +0530

    Added the products route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductsController;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', fn ()=> view('home'));
+Route::get('/products', [ProductsController::class, 'index'])->name('products.index');
